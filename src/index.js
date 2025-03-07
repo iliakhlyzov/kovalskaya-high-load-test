de2676a1ab8637ee2943f39fd01f236d3ea0b96a
iliakhlyzov-verkstedt <146713254+iliakhlyzov-verkstedt@users.noreply.github.com>
2025-03-07 14:57:10 +0300

    feat: install and setup lefthook, biome; add sequelize

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const { Sequelize } = require("sequelize");
 
 const main = () => {
-    const app = express()
+	const app = express();
 
-    app.get('/', (req, res) => {
-        res.send('Hello World!')
-    })
+	app.get("/", (req, res) => {
+		res.send("Hello World!");
+	});
 
-    app.listen(PORT, () => {
-        console.log(`Example app listening on port ${PORT}`)
-    })
-}
+	app.listen(PORT, () => {
+		console.log(`Example app listening on port ${PORT}`);
+	});
+};
 
-main()
+main();
