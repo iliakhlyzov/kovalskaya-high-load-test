30042a6a0f11f4e00af48677aa1adcc3c81758b5
iliakhlyzov-verkstedt <146713254+iliakhlyzov-verkstedt@users.noreply.github.com>
2025-03-07 14:47:42 +0300

    feat: add nodemon, docker, use node v22

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const PORT = process.env.PORT || 3000
+
+const main = () => {
+    const app = express()
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+
+    app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}`)
+    })
+}
+
+main()
