3488c52cb21a0d50ca0f1b4ba13df936a5128042
iliakhlyzov-verkstedt <146713254+iliakhlyzov-verkstedt@users.noreply.github.com>
2025-03-08 09:01:57 +0300

    feat: add migration to create table with user and 1000 balance

---
+++ migrations/add-user-table-balance-and-add-user-with-1000-credits.js
@@ -0,0 +1,53 @@
+import * as Sequelize from 'sequelize'
+
+const uuid = '39b7468a-604a-4637-b959-548ba5de3bb1'
+
+export async function up({ context: queryInterface }) {
+  await queryInterface.createTable('userBalance', {
+    id: {
+      type: Sequelize.DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.DataTypes.UUIDV4,
+    },
+    balance: {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1000,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DataTypes.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DataTypes.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+  })
+
+  await queryInterface.addConstraint('userBalance', {
+    fields: ['balance'],
+    type: 'check',
+    where: {
+      balance: {
+        [Sequelize.Op.gte]: 0,
+      },
+    },
+    name: 'balance_non_negative',
+  })
+
+  await queryInterface.bulkInsert('userBalance', [
+    {
+      id: uuid,
+      balance: 1000,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ])
+}
+
+export async function down({ context: queryInterface }) {
+  await queryInterface.dropTable('userBalance')
+}
